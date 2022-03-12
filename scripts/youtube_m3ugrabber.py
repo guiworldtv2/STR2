5a53e20820b3511cbbcc6bdc2fd8d2151c58985b
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-03-12 04:16:51 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -39,12 +39,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/vijay6672/YT2M3U/main/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/assets/moose_na.m3u')
                 return
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/vijay6672/YT2M3U/main/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/assets/moose_na.m3u')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100

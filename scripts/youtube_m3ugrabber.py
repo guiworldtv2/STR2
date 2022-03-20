59c313420369ff84d00746c248ee9cb8935c775b
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-03-20 19:17:17 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -7,33 +7,8 @@ banner = r'''
 #                                  >> https://github.com/guiworldtv       #
 ###########################################################################
 
-#EXTINF:-1  tvg-id="N/A" group-title="Argentina" tvg-logo="N/A",MTV LATINOAMERICA
-https://edge2-ccast-sl.cvattv.com.ar/live/c6eds/MTV_HD/SA_SAGEMCOM/MTV_HD.m3u8
-#EXTINF:-1  tvg-id="N/A" group-title="Argentina" tvg-logo="N/A",El Trece
-https://edge2-ccast-sl.cvattv.com.ar/live/c3eds/ArtearHD/SA_SAGEMCOM/ArtearHD.m3u8
-#EXTINF:-1  tvg-id="N/A" group-title="Argentina" tvg-logo="N/A",El Trece 2
-https://live-01-02-eltrece.vodgc.net/eltrecetv/tracks-v1a1/mono.m3u8
-#EXTINF:-1,el trece Internacional
-http://45.238.54.70:8000/play/a00a
-#EXTINF:-1,C5N
-http://45.238.54.70:8000/play/a01e
-
-#EXTINF:-1,Crðnica
-http://45.238.54.70:8000/play/a01f
-#EXTINF:-1,Encuentro
-http://45.238.54.70:8000/play/a01b
 
 
-#EXTINF:-1, tvg-id="Telefe" group-title="Argentina" tvg-name="Telefe" tvg-logo="http://www.lanoticiawebciudad.com.ar/wp-content/uploads/2016/11/telefe-logo.jpg",Telefe
-https://edge2-ccast-sl.cvattv.com.ar/live/c3eds/TelefeHD/SA_SAGEMCOM/TelefeHD.m3u8
-#EXTINF:-1,Telefe Internacional
-http://45.238.54.70:8000/play/a00b
-#EXTINF:-1  tvg-id="N/A" group-title="Argentina" tvg-logo="N/A",América TV
-https://edge2-ccast-sl.cvattv.com.ar/live/c3eds/AmericaTV/SA_SAGEMCOM/AmericaTV.m3u8
-#EXTINF:-1  tvg-id="N/A" group-title="Argentina" tvg-logo="N/A",IP - Información Periodística
-https://octubre-live.cdn.vustreams.com/live/ip/live.isml/live-audio_1=128000-video=2800000.m3u8
-#EXTINF:-1, CANAL 26 
-http://200.115.193.177/live/26hd-720/.m3u8
 '''
 
 import requests
@@ -50,12 +25,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://www.youtube.com/watch?v=YI-pQVXWH8w')
+                print('https://bit.ly/3ucp22W')
                 return
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://www.youtube.com/watch?v=YI-pQVXWH8w')
+                print('https://bit.ly/3ucp22W')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -70,10 +45,8 @@ def grab(url):
     print(f"{link[start : end]}")
 
 print('#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"')
-print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mudstein/XML/main/TIZENsiptv.xml"')
-print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/K-vanc/Tempest-EPG-Generator/main/Siteconfigs/Argentina/%5BENC%5D%5BEX%5Delcuatro.com_0.channel.xml"')
-print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Nicolas0919/Guia-EPG/master/GuiaEPG.xml"')
-    print(banner)
+print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
+print(banner)
 #s = requests.Session()
 with open('../youtube_channel_info.txt', errors="ignore") as f:
     for line in f:
@@ -93,4 +66,3 @@ with open('../youtube_channel_info.txt', errors="ignore") as f:
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')
-   

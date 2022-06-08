5eb18b0026547c537977ccca52223dcf58495978
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-08 11:00:56 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -33,6 +33,82 @@ https://edge2-ccast-sl.cvattv.com.ar/live/c3eds/AmericaTV/SA_SAGEMCOM/AmericaTV.
 https://octubre-live.cdn.vustreams.com/live/ip/live.isml/live-audio_1=128000-video=2800000.m3u8
 #EXTINF:-1, CANAL 26 
 http://200.115.193.177/live/26hd-720/.m3u8
+#EXTINF:-1, A24 FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27183
+#EXTINF:-1, A24 HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27182
+#EXTINF:-1, America TV FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27180
+#EXTINF:-1, America TV HD
+http://goldiptv.online:8080/mevlut/mevlut123/27179
+#EXTINF:-1, C5N HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27178
+#EXTINF:-1, Canal 26 Noticias HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27177
+#EXTINF:-1, Canal 4 Esquel
+http://goldiptv.online:8080/mevlut/mevlut123/27176
+#EXTINF:-1, Canal 9 Salta
+http://goldiptv.online:8080/mevlut/mevlut123/27175
+#EXTINF:-1, CINE.AR HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27174
+#EXTINF:-1, CN23 HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27173
+#EXTINF:-1, Cronica Television HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27172
+#EXTINF:-1, DeporTV FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27171
+#EXTINF:-1, DeporTV HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27170
+#EXTINF:-1, El Garage TV HD
+http://goldiptv.online:8080/mevlut/mevlut123/27169
+#EXTINF:-1, El Nueve FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27168
+#EXTINF:-1, El Nueve HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27167
+#EXTINF:-1, El Rural HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27166
+#EXTINF:-1, El trece FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27165
+#EXTINF:-1, El trece HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27164
+#EXTINF:-1, Fox Sports 1
+http://goldiptv.online:8080/mevlut/mevlut123/27162
+#EXTINF:-1, Fox Sports 2
+http://goldiptv.online:8080/mevlut/mevlut123/27161
+#EXTINF:-1, Fox Sports Premium FHD
+http://goldiptv.online:8080/mevlut/mevlut123/27160
+#EXTINF:-1, Fox Sports Premium HD
+http://goldiptv.online:8080/mevlut/mevlut123/27159
+#EXTINF:-1, LN Mas HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27158
+#EXTINF:-1, Net TV FHD
+http://goldiptv.online:8080/mevlut/mevlut123/27157
+#EXTINF:-1, Net TV HD
+http://goldiptv.online:8080/mevlut/mevlut123/27156
+#EXTINF:-1, Telefe FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27152
+#EXTINF:-1, Telefe HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27151
+#EXTINF:-1, Telemax
+http://goldiptv.online:8080/mevlut/mevlut123/27150
+#EXTINF:-1, Television Publica FHD*
+http://goldiptv.online:8080/mevlut/mevlut123/27149
+#EXTINF:-1, Television Publica HD*
+http://goldiptv.online:8080/mevlut/mevlut123/27148
+#EXTINF:-1, TN Buenos Aires
+http://goldiptv.online:8080/mevlut/mevlut123/27147
+#EXTINF:-1, TNT Sports HD
+http://goldiptv.online:8080/mevlut/mevlut123/27146
+#EXTINF:-1, TR Telered
+http://goldiptv.online:8080/mevlut/mevlut123/27145
+#EXTINF:-1, Trece Max HD
+http://goldiptv.online:8080/mevlut/mevlut123/27144
+#EXTINF:-1, TyC Sports FHD
+http://goldiptv.online:8080/mevlut/mevlut123/27143
+#EXTINF:-1, TyC Sports HD
+http://goldiptv.online:8080/mevlut/mevlut123/27142
+#EXTINF:-1, TyC Sports Internacional
+http://goldiptv.online:8080/mevlut/mevlut123/27141
 
 '''
 

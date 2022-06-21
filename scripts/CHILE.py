62b543936bd8b5ee24f76c05f32739e5742a375d
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-21 02:55:02 -0300

    Update CHILE.py

--- scripts/CHILE.py
+++ scripts/CHILE.py
@@ -60,12 +60,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://eu-nl-012.worldcast.tv/dancetelevisionone/2/dancetelevisionone.m3u8')
+                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAirYT.m3u')
                 return
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://eu-nl-012.worldcast.tv/dancetelevisionone/2/dancetelevisionone.m3u8')
+                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAirYT.m3u')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100

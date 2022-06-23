36e701ef888867588abd8ee0a448fd9eae63ea64
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-22 22:54:22 -0300

    Update DAILYMOTION.sh

--- DAILYMOTION.sh
+++ DAILYMOTION.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 echo $(dirname $0)
+
+python3 -m pip install requests
+
 cd $(dirname $0)/scripts/
-python3 DAILYMOTION.py > ../DAILYMOTION.m3u8
+
+python3 DAILYMOTION.py > ../DAILYMOTION.py
 
 echo m3u grabbed
+

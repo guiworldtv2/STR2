7346be84be7e9cc6196995e8059e5559a3db7794
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-27 10:47:12 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -803,27 +803,7 @@ def get_session(video_id, token):
     
     
     
-    
-    #! /usr/bin/python3
-import requests
-import os
 
-def grab(url):
-    response = requests.get(url, timeout = 15).text
-    if '.m3u8' not in response:
-        print("https://bit.ly/3xRjEnQ")
-        return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end - tuner : end]:
-            link = response[end - tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
 
 with open('./GLOBO.txt', encoding='utf-8') as f:
     for line in f:

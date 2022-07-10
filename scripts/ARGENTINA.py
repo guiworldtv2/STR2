dc88e7c91d00f4937c02f94360b7a4bf93e80644
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-10 03:29:02 -0300

    Update ARGENTINA.py

--- scripts/ARGENTINA.py
+++ scripts/ARGENTINA.py
@@ -1,11 +1,7 @@
 #! /usr/bin/python3
 
 banner = r'''
-###########################################################################
-#                                                                         #
 
-#                                  >> https://github.com/guiworldtv       #
-###########################################################################
 
 #EXTINF:-1 group-title="Argentina tvg-logo=",AMC ARGENTINA
 https://raw.githubusercontent.com/fgillusi/IPTV-Argentina/main/AMC.m3u8
@@ -293,7 +289,7 @@ print('#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml
 print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mudstein/XML/main/TIZENsiptv.xml"')
 print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/K-vanc/Tempest-EPG-Generator/main/Siteconfigs/Argentina/%5BENC%5D%5BEX%5Delcuatro.com_0.channel.xml"')
 print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Nicolas0919/Guia-EPG/master/GuiaEPG.xml"')
-print(banner)
+
 #s = requests.Session()
 with open('../ARGENTINA.txt', errors="ignore") as f:
     for line in f:
@@ -309,7 +305,7 @@ with open('../ARGENTINA.txt', errors="ignore") as f:
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
             grab(line)
-            
+print(banner)            
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')

dd52508206c7b74672284d7e546b23e937b33b47
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-15 00:03:34 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -1,109 +1,3 @@
-from __future__ import unicode_literals
-import youtube_dl
-import requests
-import shutil
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-channel_no = 0
-m3u = None
-def get_live_info(channel_id):
-    try:
-        webpage = urlopen(f"https://www.youtube.com/{channel_id}/live").read()
-        soup = BeautifulSoup(webpage, 'html.parser')
-        urlMeta = soup.find("meta", property="og:url")
-        if urlMeta is None:
-            return None
-        url = urlMeta.get("content")
-        if(url is None or url.find("/watch?v=") == -1):
-            return None
-        titleMeta = soup.find("meta", property="og:title")
-        imageMeta = soup.find("meta", property="og:image")
-        descriptionMeta = soup.find("meta", property="og:description")
-        return {
-            "url": url,
-            "title": titleMeta.get("content"),
-            "image": imageMeta.get("content"),
-            "description": descriptionMeta.get("content")
-        }
-    
-    except Exception as e:
-                return None
-
-def generate_youtube_tv():
-    global channel_no
-    ydl_opts = {
-        'format': 'best',
-    }
-    ydl = youtube_dl.YoutubeDL(ydl_opts)
-
-    with open('GLOBO.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            if line == "":
-                continue
-            channel = get_live_info(line)
-            if channel is None:
-                continue
-            try:
-                with ydl:
-                    result = ydl.extract_info(
-                        f"https://www.youtube.com/{line}/live",
-                        download=False  # We just want to extract the info
-                    )
-
-                    if 'entries' in result:
-                        # Can be a playlist or a list of videos
-                        video = result['entries'][-1]
-                    else:
-                        # Just a video
-                        video = result
-                video_url = video['url']
-
-                channel_no += 1
-                channel_name = f"{channel_no}-{line.split('/')[-1]}"
-                playlistInfo = f"#EXTINF:-1 tvg-chno=\"{channel_no}\" tvg-id=\"{line}\" tvg-name=\"{channel_name}\" tvg-logo=\"{channel.get('image')}\" group-title=\"ARGENTINA\",{channel_name}\n"
-                write_to_playlist(playlistInfo)
-                write_to_playlist(video_url)
-                write_to_playlist("\n")
-            except Exception as e:
-                print(e)
-                        
-
-
-
-def write_to_playlist(content):
-    global m3u    
-    m3u.write(content)
-    
-
-def create_playlist():
-    global m3u
-    m3u = open("ARGENTINA.m3u", "w")
-    m3u.write("#EXTM3U")
-    m3u.write("\n")
-    
-    
-def close_playlist():
-    global m3u
-    m3u.close()
-def generate_youtube_PlayList():
-    create_playlist()
-        
-    
-    generate_youtube_tv()
-    
-    m3u.write(banner)
-    
-    
-    close_playlist()
-
-
-    
-if __name__ == '__main__':
-    generate_youtube_PlayList()
-
-    
 banner = r'''
 ###########################################################################
 #                                                                         #
@@ -447,4 +341,110 @@ http://www.trimi.com.ar/santa_maria/streaming/mystreamSantaMariaHSMed.m3u8
 https://g3.mc-hor.transport.edge-access.net/a09/ngrp:gcba_video3-100042_all/gcba_video3-100042_720p.m3u8
 #EXTINF:-1, CANAL 26 
 http://200.115.193.177/live/26hd-720/.m3u8
-'''    
+''' 
+
+from __future__ import unicode_literals
+import youtube_dl
+import requests
+import shutil
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+channel_no = 0
+m3u = None
+def get_live_info(channel_id):
+    try:
+        webpage = urlopen(f"https://www.youtube.com/{channel_id}/live").read()
+        soup = BeautifulSoup(webpage, 'html.parser')
+        urlMeta = soup.find("meta", property="og:url")
+        if urlMeta is None:
+            return None
+        url = urlMeta.get("content")
+        if(url is None or url.find("/watch?v=") == -1):
+            return None
+        titleMeta = soup.find("meta", property="og:title")
+        imageMeta = soup.find("meta", property="og:image")
+        descriptionMeta = soup.find("meta", property="og:description")
+        return {
+            "url": url,
+            "title": titleMeta.get("content"),
+            "image": imageMeta.get("content"),
+            "description": descriptionMeta.get("content")
+        }
+    
+    except Exception as e:
+                return None
+
+def generate_youtube_tv():
+    global channel_no
+    ydl_opts = {
+        'format': 'best',
+    }
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
+
+    with open('GLOBO.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                continue
+            channel = get_live_info(line)
+            if channel is None:
+                continue
+            try:
+                with ydl:
+                    result = ydl.extract_info(
+                        f"https://www.youtube.com/{line}/live",
+                        download=False  # We just want to extract the info
+                    )
+
+                    if 'entries' in result:
+                        # Can be a playlist or a list of videos
+                        video = result['entries'][-1]
+                    else:
+                        # Just a video
+                        video = result
+                video_url = video['url']
+
+                channel_no += 1
+                channel_name = f"{channel_no}-{line.split('/')[-1]}"
+                playlistInfo = f"#EXTINF:-1 tvg-chno=\"{channel_no}\" tvg-id=\"{line}\" tvg-name=\"{channel_name}\" tvg-logo=\"{channel.get('image')}\" group-title=\"ARGENTINA\",{channel_name}\n"
+                write_to_playlist(playlistInfo)
+                write_to_playlist(video_url)
+                write_to_playlist("\n")
+            except Exception as e:
+                print(e)
+                        
+
+
+
+def write_to_playlist(content):
+    global m3u    
+    m3u.write(content)
+    
+
+def create_playlist():
+    global m3u
+    m3u = open("ARGENTINA.m3u", "w")
+    m3u.write("#EXTM3U")
+    m3u.write("\n")
+    
+    
+def close_playlist():
+    global m3u
+    m3u.close()
+def generate_youtube_PlayList():
+    create_playlist()
+        
+    
+    generate_youtube_tv()
+    
+    m3u.write(banner)
+    
+    
+    close_playlist()
+
+
+    
+if __name__ == '__main__':
+    generate_youtube_PlayList()   
+ 

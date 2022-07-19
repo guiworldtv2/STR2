62b7fd85eceaa7a989b4b7e877706b349be5f3d3
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-19 00:33:31 -0300

    Update ARGENTINA.py

--- scripts/ARGENTINA.py
+++ scripts/ARGENTINA.py
@@ -446,12 +446,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                None in line[0] line[1] line[2]line[3]
+                None 
                 return 
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                None in line[0] line[1] line[2]line[3]
+                None 
                 return
     end = response.find('.m3u8') + 5
     tuner = 100

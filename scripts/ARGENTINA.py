8780c21b3a66316261fd1483f8a6cef99d2bf18b
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-18 23:53:31 -0300

    Update ARGENTINA.py

--- scripts/ARGENTINA.py
+++ scripts/ARGENTINA.py
@@ -446,12 +446,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAir.m3u8')
+                None
                 return
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAir.m3u8')
+                None
                 return
     end = response.find('.m3u8') + 5
     tuner = 100

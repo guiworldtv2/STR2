c5da45a6233d617278edf20ee9a4faddf26b3128
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-12-13 02:48:53 -0300

    Update BIG_BROTHER_PORTUGAL_LIVE.sh

--- BIG_BROTHER_PORTUGAL_LIVE.sh
+++ BIG_BROTHER_PORTUGAL_LIVE.sh
@@ -2,6 +2,6 @@
 
 # TVI Reality - update the stream URL of TVI Reality
 
-sed -i "/live_tvi_direct/ c https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
+sed -i "/live_tvi_direct/ c https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" BIG_BROTHER_PORTUGAL_LIVE.M3U8
 
 exit 0

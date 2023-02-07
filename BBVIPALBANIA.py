d6b34f834ceb2d58095b5acca70fbff16cb3d56f
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-07 04:55:58 -0300

    Create BBVIPALBANIA.py

---
+++ BBVIPALBANIA.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import streamlink
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+m3u8_file = open("AFAZENDA.m3u8", "w")
+
+for i in range(1, 3):
+    url = f"https://tviplayer.iol.pt/videos/ultimos/{i}/canal:"
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    video_titles = [item.text for item in soup.find_all("span", class_="item-title")]
+    video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", class_="item")]
+    Data = [item.text for item in soup.find_all("span", class_="item-date")]
+
+    for title, link, data in zip(video_titles, video_links, Data):
+        timestamp = datetime.datetime.now().strftime("%m%d%H%M%S")
+        video_url = streamlink.streams(link)["best"].url
+        m3u8_file.write(f"#EXTINF:-1,{data}{timestamp}_SBTVD_{title}_-ANO\n{video_url}\n")
+
+m3u8_file.close()

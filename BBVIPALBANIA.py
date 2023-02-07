85c42470fd772c28053f14e6ce954ae9e18a1dcc
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-07 06:59:54 -0300

    Update BBVIPALBANIA.py

--- BBVIPALBANIA.py
+++ BBVIPALBANIA.py
@@ -1,20 +1,20 @@
 import streamlink
 
-with open("BBVIPALBANIA.txt", "r") as f:
-    links = f.readlines()
+session = streamlink.Streamlink()
+session.set_option("twitch-disable-ads", True)
+session.set_option("twitch-disable-reruns", True)
 
-for link in links:
-    session = streamlink.Streamlink()
-    session.set_option("twitch-disable-ads", True)
-    session.set_option("twitch-disable-reruns", True)
-    
-    video_url = session.streams(link.strip())["best"].url if session.streams(link.strip()) else None
-
-    if video_url:
-        with open("BBVIPALBANIA.m3u8", "w") as m3u8_file:
+with open("BBVIPALBANIA.txt") as links_file:
+    for link in links_file:
+        link = link.strip()
+        video_url = session.streams(link)["best"].url if session.streams(link) else None
+        if video_url:
+            m3u8_file = open(f"{link}.m3u8", "w")
             m3u8_file.write("#EXTM3U\n")
             m3u8_file.write("#EXT-X-VERSION:3\n")
             m3u8_file.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5400000\n")
             m3u8_file.write(video_url + "\n")
+            m3u8_file.close()
 
-    session.close()
+session.close()
+

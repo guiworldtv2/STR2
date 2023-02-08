84c6657e9dc399d6c2171fe37675a5db6d5caf82
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-08 05:53:38 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -3,11 +3,24 @@ from bs4 import BeautifulSoup
 import datetime
 import streamlink
 
+import re
+
+
+
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
 
 m3u8_file = open("AFAZENDA.m3u8", "w")
+
+def format_date(data):
+    data = re.sub("(seg|ter|qua|qui|sex|sab|dom)", "", data)
+    data = data.strip()
+    if len(data) == 1:
+        data = "0" + data
+    data = re.sub(r"(\d+) de ([a-z]+) de (\d+)", r"\2\1\3", data)
+    return data
 
 for i in range(1, 3):
     url = f"https://tviplayer.iol.pt/videos/ultimos/{i}/canal:"

2ed06ef46be0a6dd9504e2662142e851ea3a7f35
GitHub Action <action@github.com>
2023-02-10 10:53:30 +0000

    links atualizados

--- VideoOFFAir.ts
+++ VideoOFFAir.ts
@@ -1,2 +1,9 @@
 
-              
+ [cli][info] Found matching plugin youtube for URL https://www.youtube.com/watch?v=MYU3GEeNE7U
+[stream.ffmpegmux][warning] No valid FFmpeg binary was found. See the --ffmpeg-ffmpeg option.
+[stream.ffmpegmux][warning] Muxing streams is unsupported! Only a subset of the available streams can be returned!
+[cli][info] Available streams: audio_mp4a, audio_opus, 360p (worst, best)
+[cli][info] Opening stream: 360p (http)
+[cli][info] Writing output to
+/home/runner/work/STR2/STR2/VideoOFFAir.ts
+[cli][error] File VideoOFFAir.ts already exists, use --force to overwrite it.             

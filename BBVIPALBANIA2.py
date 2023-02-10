4a77ec4d4754ff5b9cddf2c725a15fbd1d738bfd
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-10 10:51:46 -0300

    Update BBVIPALBANIA2.py

--- BBVIPALBANIA2.py
+++ BBVIPALBANIA2.py
@@ -1,23 +1,27 @@
 import requests
-import time
 from bs4 import BeautifulSoup
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
-}
+import time
 
 url = "https://www.twitch.tv/search?term=bbvip"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+}
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
+def get_links(url, headers):
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, "html.parser")
+    links = []
+    for link in soup.find_all("a", class_="ScCoreLink-sc-16kq0mq-0 fQAQWb tw-link"):
+        href = link.get("href")
+        links.append(href)
+    return links
 
-links = []
+def save_to_file(links):
+    with open("BBVIPALBANIA.txt", "w") as f:
+        for link in links:
+            f.write("https://www.twitch.tv" + link + "\n")
 
-for link in soup.find_all("a", {"class": "ScCoreLink-sc-16kq0mq-0 fQAQWb tw-link"}):
-    links.append("https://www.twitch.tv" + link["href"])
-
-time.sleep(10)
-
-with open("BBVIPALBANIA.txt", "w") as file:
-    for link in links:
-        file.write(link + "\n")
+if __name__ == "__main__":
+    time.sleep(10)
+    links = get_links(url, headers)
+    save_to_file(links)

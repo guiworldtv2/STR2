aff85e0db75b869ba1a20fba2492a38155619ac9
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-10 10:39:21 -0300

    Update BBVIPALBANIA2.py

--- BBVIPALBANIA2.py
+++ BBVIPALBANIA2.py
@@ -15,8 +15,10 @@ links = soup.find_all("a", class_="ScCoreLink-sc-16kq0mq-0 fQAQWb tw-link")
 formatted_links = ["https://www.twitch.tv" + link["href"] for link in links]
 
 # Escreve os links formatados em um arquivo chamado "BBVIPALBANIA.txt"
-with open("BBVIPALBANIA.txt", "w") as file:
-    for link in formatted_links:
-        file.write(link + "\n")
-
-print("Links capturados e escritos no arquivo BBVIPALBANIA.txt.")
+try:
+    with open("BBVIPALBANIA.txt", "w") as file:
+        for link in formatted_links:
+            file.write(link + "\n")
+    print("Links capturados e escritos no arquivo BBVIPALBANIA.txt.")
+except:
+    print("Ocorreu um erro ao escrever no arquivo.")

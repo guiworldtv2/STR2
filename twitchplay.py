7f4c225d018663c154cf76ec15897bfc347f0db4
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-14 11:35:53 -0300

    Update twitchplay.py

--- twitchplay.py
+++ twitchplay.py
@@ -73,7 +73,7 @@ try:
 
             # Write the stream information to the file
             title = channels[i]
-            f.write(f"#EXTINF:-1 tvg-id='{title}' group-title=\"TWITCH\",{title2}\n")           
+            f.write(f"#EXTINF:-1 tvg-id='{title}' group-title=\"TWITCH\",{title}\n")           
             f.write(f"{url}\n")
             f.write("\n")
 except Exception as e:

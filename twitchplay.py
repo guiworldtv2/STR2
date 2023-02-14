0d4fe4f9ecce6469ed431a92367096959dde4c01
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-14 13:10:59 -0300

    Update twitchplay.py

--- twitchplay.py
+++ twitchplay.py
@@ -1,3 +1,42 @@
+import streamlink
+import subprocess
+import time
+import os
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Configuring Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+
+# Instanciando o driver do Chrome
+driver = webdriver.Chrome(options=chrome_options)
+
+# URL da página desejada
+url_twitch = "https://www.twitch.tv/"
+
+# Abrir a página desejada
+driver.get(url_twitch)
+
+# Aguardar alguns segundos para carregar todo o conteúdo da página
+time.sleep(5)
+
+# Scroll to the bottom of the page using ActionChains
+while True:
+    try:
+        # Find the last video on the page
+        last_video = driver.find_element_by_xpath("//a[@class='ScCoreLink-sc-16kq0mq-0 jKBAWW tw-link'][last()]")
+        # Scroll to the last video
+        actions = ActionChains(driver)
+        actions.move_to_element(last_video).perform()
+        time.sleep(1)
+    except:
+        break
+        
+        
 # Get the page source again after scrolling to the bottom
 html_content = driver.page_source
 

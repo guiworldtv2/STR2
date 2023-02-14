4f1d68a7f73c545e246321baf041926d4804dfec
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-13 22:34:28 -0300

    Update BBVIPALBANIA.py

--- BBVIPALBANIA.py
+++ BBVIPALBANIA.py
@@ -6,44 +6,44 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
-        # URL da página desejada
-        url_twitch = "https://www.twitch.tv/search?term=big%20brother"
+# URL da página desejada
+url_twitch = "https://www.twitch.tv/search?term=big%20brother"
 
-        # Instanciando o driver do firefox
-        driver = webdriver.Firefox()
+# Instanciando o driver do firefox
+driver = webdriver.Firefox()
 
-        # Abrir a página desejada
-        driver.get(url_twitch)
+# Abrir a página desejada
+driver.get(url_twitch)
 
-        # Aguardar alguns segundos para carregar todo o conteúdo da página
-        time.sleep(5)
+# Aguardar alguns segundos para carregar todo o conteúdo da página
+time.sleep(5)
 
-        # Obter o conteúdo da página
-        html_content = driver.page_source
+# Obter o conteúdo da página
+html_content = driver.page_source
 
-        # Encontrar o link do primeiro canal encontrado
-        soup = BeautifulSoup(html_content, "html.parser")
-        link = "https://www.twitch.tv" + soup.find("a", class_="ScCoreLink-sc-16kq0mq-0 eYjhIv tw-link").get("href")
+# Encontrar o link do primeiro canal encontrado
+soup = BeautifulSoup(html_content, "html.parser")
+link = "https://www.twitch.tv" + soup.find("a", class_="ScCoreLink-sc-16kq0mq-0 eYjhIv tw-link").get("href")
 
-        # Fechar o driver
-        driver.quit()
+# Fechar o driver
+driver.quit()
 
-        print(link)
+print(link)
 
-        # Instalar streamlink (somente se necessário)
-        subprocess.run(["pip", "install", "--user", "--upgrade", "streamlink"], check=True)
+# Instalar streamlink (somente se necessário)
+subprocess.run(["pip", "install", "--user", "--upgrade", "streamlink"], check=True)
 
-        # Criar o arquivo BBVIPALBANIA.m3u8
-        with open("./BBVIPALBANIA.m3u8", "w") as f:
-            f.write("#EXTM3U\n")
-            f.write("#EXT-X-VERSION:3\n")
-            f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5400000\n")
+# Criar o arquivo BBVIPALBANIA.m3u8
+with open("./BBVIPALBANIA.m3u8", "w") as f:
+    f.write("#EXTM3U\n")
+    f.write("#EXT-X-VERSION:3\n")
+    f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5400000\n")
 
-            # Executar streamlink e obter o URL do stream
-            result = subprocess.run(["streamlink", "--twitch-disable-ads", "--twitch-disable-reruns", "--default-stream", "best", link], capture_output=True, text=True)
-            if result.returncode == 0:
-                stream_url = result.stdout.strip()
-                f.write(stream_url + "\n")
-            else:
-                f.write("https://raw.githubusercontent.com/guiworldtv/STR2/main/VideoOFFAir.m3u8\n")
-        time.sleep(30)
+# Executar streamlink e obter o URL do stream
+result = subprocess.run(["streamlink", "--twitch-disable-ads", "--twitch-disable-reruns", "--default-stream", "best", link], capture_output=True, text=True)
+if result.returncode == 0:
+    stream_url = result.stdout.strip()
+    f.write(stream_url + "\n")
+else:
+f.write("https://raw.githubusercontent.com/guiworldtv/STR2/main/VideoOFFAir.m3u8\n")
+time.sleep(30)

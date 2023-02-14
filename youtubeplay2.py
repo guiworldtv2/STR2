d0934ef9fe5b0a1bf8d4ef6b829eb166193bd919
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-14 20:16:22 -0300

    Update youtubeplay2.py

--- youtubeplay2.py
+++ youtubeplay2.py
@@ -16,7 +16,7 @@ chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 # URL da página desejada
-url_youtube = "https://www.youtube.com/channel/UCYfdidRxbB8Qhf0Nx7ioOYw"
+url_youtube = "https://www.youtube.com/results?search_query=podcast&sp=CAISAhgC"
 
 # Abrir a página desejada
 driver.get(url_youtube)
@@ -45,15 +45,9 @@ time.sleep(5)
 # Find the links and titles of the videos found
 try:
     soup = BeautifulSoup(html_content, "html.parser")
-    videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-grid-video-renderer")
-    links = [f"https://www.youtube.com{video.get('href')}" for video in videos]
-    titles = []
-    for video in videos:
-        text = video.get_text()
-        title_start = text.index('title="') + len('title="')
-        title_end = text.index('"', title_start)
-        title = text[title_start:title_end]
-        titles.append(title)
+    videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-video-renderer")
+    links = ["https://www.youtube.com" + video.get("href") for video in videos]
+    titles = [video.get("title") for video in videos]
 except Exception as e:
     print(f"Erro: {e}")
 finally:
@@ -63,28 +57,45 @@ finally:
 
 
 
-
 # Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
+subprocess.run(['pip', 'install', '--user', '--upgrade', 'youtube-dl'])
 
-print(title)
-print(url)
 
 time.sleep(5)
 
-# Get the playlist and write to file
+# Define as opções para o youtube-dl
+ydl_opts = {
+    'format': 'best',   # Obtém a melhor qualidade
+    'merge_output_format': 'm3u8',  # Obtém a url com o formato .m3u8
+    'write_all_thumbnails': True,  # Grava todas as thumbnails em arquivos separados
+    'outtmpl': '%(id)s.%(ext)s',  # Nome do arquivo de saída
+}
+
 try:
-    with open('./YOUTUBEPLAY2.m3u', 'w') as f:
+    with open('./YOUTUBEPLAY.m3u', 'w') as f:
         f.write("#EXTM3U\n")  # Imprime #EXTM3U uma vez no início do arquivo
         for i, link in enumerate(links):
-            # Get the stream information using streamlink
-            streams = streamlink.streams(link)
-            url = streams['best'].url
-            # Write the stream information to the file
-            title = titles[i]
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                # Obtém informações do vídeo com o youtube-dl
+                info_dict = ydl.extract_info(link, download=False)
 
-            f.write(f"#EXTINF:-1 group-title=\"YOUTUBE\",{title}\n")
-            f.write(f"{url}\n\n")
-            f.write("\n")            
+                # Obtém a url do vídeo com o formato .m3u8
+                url = info_dict['url']
+
+                # Obtém a thumbnail do vídeo
+                thumbnail_url = info_dict['thumbnails'][0]['url']
+
+                # Grava a thumbnail em um arquivo
+                thumbnail_filename = f"{info_dict['id']}.jpg"
+                with open(thumbnail_filename, 'wb') as thumbnail_file:
+                    thumbnail_file.write(ydl.urlopen(thumbnail_url).read())
+
+                # Escreve as informações do vídeo no arquivo .m3u8
+                title = titles[i]
+                f.write(f"#EXTINF:-1 group-title=\"YOUTUBE\",{title}\n")
+                f.write(f"#EXTVLCOPT:program=128\n")  # Define o programa para 128
+                f.write(f"{url}\n\n")
+                f.write(f"#EXTVLCOPT:thumbnail-file={thumbnail_filename}\n")  # Adiciona o caminho para o arquivo da thumbnail
+                f.write("\n")
 except Exception as e:
     print(f"Erro ao criar o arquivo .m3u8: {e}")

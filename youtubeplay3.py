ce3960b6ae62d6fde5d8912ceffc23f3932fcc99
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-15 11:50:20 -0300

    Update youtubeplay3.py

--- youtubeplay3.py
+++ youtubeplay3.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
 # Configuring Chrome options
 chrome_options = Options()
@@ -15,41 +16,48 @@ chrome_options.add_argument("--disable-gpu")
 # Instanciando o driver do Chrome
 driver = webdriver.Chrome(options=chrome_options)
 
-# Counter to name the downloaded videos
-counter = 1
-
-for page in range(1, 11): # loop through pages 1 to 10
-    
-# URL da página desejada
-url_vimeo = "https://vimeo.com/search/page:{page}/sort:latest?duration=long&q=aula"
-
-# Abrir a página desejada
-driver.get(url_youtube)
-
-# Aguardar alguns segundos para carregar todo o conteúdo da página
-time.sleep(5)
-
 # Scroll to the bottom of the page using ActionChains
 while True:
     try:
-        # Find all <a> elements with class "iris_video-vital__overlay"
-    videos = driver.find_elements(By.CSS_SELECTOR, "a.iris_video-vital__overlay")
+        for page in range(1, 12):
+        # URL da página desejada
+        url_vimeo = f"https://vimeo.com/search/page:{page}/sort:latest?duration=long&q=aula"
 
-    # Store the links of the found videos
-    video_links = []
-    for video in videos:
-        video_links.append(video.get_attribute("href"))
+        # Abrir a página desejada
+        driver.get(url_vimeo)
 
-    # Find all <span> elements with class "iris_link iris_link--gray-2"
-    video_titles = driver.find_elements(By.CSS_SELECTOR, "span.iris_link.iris_link--gray-2")
+        # Aguardar alguns segundos para carregar todo o conteúdo da página
+        time.sleep(5)
 
-    # Store the titles of the found videos
-    video_titles_list = []
-    for title in video_titles:
-        video_titles_list.append(title.get_attribute("title"))
+        # Find all <a> elements with class "js-preview-link"
+        videos_found = driver.find_elements(By.CSS_SELECTOR, "a.js-preview-link")
 
-    # Dictionary with links and titles of the videos
-    video_dict = dict(zip(video_links, video_titles_list))
+        # Store the links of the found videos
+        video_links = []
+        for video in videos_found:
+            video_links.append(video.get_attribute("href"))
+
+        # Find all <h3> elements with class "preview__title"
+        video_titles = driver.find_elements(By.CSS_SELECTOR, "h3.preview__title")
+
+        # Store the titles of the found videos
+        video_titles_list = []
+        for title in video_titles:
+            video_titles_list.append(title.text)
+
+        # Dictionary with links and titles of the videos
+        video_dict = dict(zip(video_links, video_titles_list))
+        
+        # If no more videos are found, break the loop
+        if len(videos_found) == 0:
+            break
+
+        # Scroll down to load more videos
+        actions = ActionChains(driver)
+        actions.move_to_element(videos_found[-1])
+        actions.perform()
+
+        time.sleep(5)
     except:
         break
         
@@ -59,12 +67,15 @@ html_content = driver.page_source
 
 time.sleep(5)
 
-# Find the links and titles of the videos found
 try:
     soup = BeautifulSoup(html_content, "html.parser")
-    videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-video-renderer")
-    links = ["https://www.youtube.com" + video.get("href") for video in videos]
+    videos = soup.find_all("a", class_="iris_video-vital__overlay")
+    links = [video.get("href") for video in videos]
     titles = [video.get("title") for video in videos]
+    print("Links dos vídeos encontrados:")
+    print(links)
+    print("Títulos dos vídeos encontrados:")
+    print(titles)
 except Exception as e:
     print(f"Erro: {e}")
 finally:
@@ -75,37 +86,23 @@ finally:
 
 
 # Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'youtube-dl'])
-
+subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
 
 time.sleep(5)
 
-# Define as opções para o youtube-dl
-ydl_opts = {
-    'format': 'best',  # Obtém a melhor qualidade
-    'merge_output_format': 'm3u8',  # Obtém a url com o formato .m3u8
-    'write_all_thumbnails': False,  # Não faz download das thumbnails
-    'skip_download': True,  # Não faz download do vídeo
-}
-
+# Get the playlist and write to file
 try:
-    with open('./YOUTUBEPLAY3.m3u', 'w') as f:
+    with open('./VIMEOPLAY1.m3u', 'w') as f:
         f.write("#EXTM3U\n")  # Imprime #EXTM3U uma vez no início do arquivo
         for i, link in enumerate(links):
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                # Obtém informações do vídeo com o youtube-dl
-                info_dict = ydl.extract_info(link, download=False)
+            # Get the stream information using streamlink
+            streams = streamlink.streams(link)
+            url = streams['best'].url
+            # Write the stream information to the file
+            title = titles[i]
 
-                # Obtém a url do vídeo com o formato .m3u8
-                url = info_dict['url']
-
-                # Obtém a url da thumbnail do vídeo
-                thumbnail_url = info_dict['thumbnail']
-
-                # Escreve as informações do vídeo no arquivo .m3u8
-                title = titles[i]
-                f.write(f"#EXTINF:-1 group-title=\"YOUTUBE3\" tvg-logo=\"{thumbnail_url}\",{title}\n")
-                f.write(f"{url}\n\n")
-                f.write("\n")
+            f.write(f"#EXTINF:-1 group-title=\"VIMEO1\",{title}\n")
+            f.write(f"{url}\n\n")
+            f.write("\n")            
 except Exception as e:
     print(f"Erro ao criar o arquivo .m3u8: {e}")

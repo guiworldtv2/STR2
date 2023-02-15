9e2f825ce86a99a02984030688c37ac302dec48e
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-15 19:18:31 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -1,78 +1,19 @@
-import streamlink
-import subprocess
-import time
-import os
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 
-# Configuring Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-
-# Instanciando o driver do Chrome
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=chrome_options)
 
-# URL da página desejada
-url_twitch = "https://www.twitch.tv/search?term=zadruga"
+driver.get("https://youtube.com/")
+titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
+views = driver.find_elements(By.XPATH, '//*[@id="metadata-line"]')
 
-# Abrir a página desejada
-driver.get(url_twitch)
+for i in range(len(views)):
+    print(titles[i].text)
+    print(views[i].text, '\n')
 
-# Aguardar alguns segundos para carregar todo o conteúdo da página
-time.sleep(5)
-
-# Scroll to the bottom of the page using ActionChains
-while True:
-    try:
-        # Find the last video on the page
-        last_video = driver.find_element_by_xpath("//a[@class='ScCoreLink-sc-16kq0mq-0 jKBAWW tw-link'][last()]")
-        # Scroll to the last video
-        actions = ActionChains(driver)
-        actions.move_to_element(last_video).perform()
-        time.sleep(1)
-    except:
-        break
-        
-        
-# Get the page source again after scrolling to the bottom
-html_content = driver.page_source
-
-# Find the links and titles of the videos found
-try:
-    soup = BeautifulSoup(html_content, "html.parser")
-    videos = soup.find_all("a", class_="ScCoreLink-sc-16kq0mq-0 jKBAWW tw-link", href=True)
-    links = ["https://www.twitch.tv" + video.get("href") for video in videos]
-    channels = [video.find("p", {"data-a-target": "preview-card-channel-link", "class": "CoreText-sc-1txzju1-0 jiepBC"}).get("title") for video in videos]
-    titles = [video.find("h3", class_="CoreText-sc-1txzju1-0 eJuFGD").get("title") for video in videos]
-    thumbnails = soup.find_all('img', class_='search-result-card__img')
-except Exception as e:
-    print(f"Erro: {e}")
-finally:
-    # Close the driver
-    driver.quit()
-
-
-
-
-# Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
-
-# Get the playlist and write to file
-try:
-    with open('./TWITCHPLAY.m3u', 'w') as f:
-        f.write("#EXTM3U\n")  # Imprime #EXTM3U uma vez no início do arquivo
-        for i, link in enumerate(links):
-            # Get the stream information using streamlink
-            streams = streamlink.streams(link)
-            url = streams['best'].url
-        for thumbnail in thumbnails:
-            # Write the stream information to the file
-            title = channels[i]
-            f.write(f"#EXTINF:-1 tvg-id='{title}' tvg-logo='{thumbnail['src']}' group-title=\"TWITCH\",{title}\n")           
-            f.write(f"{url}\n")
-            f.write("\n")
-except Exception as e:
-    print(f"Erro ao criar o arquivo .m3u8: {e}")
+driver.close()

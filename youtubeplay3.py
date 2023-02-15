b48de6c5c1c3644db98276bcd7f1bbb80b8eccaf
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-15 14:55:36 -0300

    Update youtubeplay3.py

--- youtubeplay3.py
+++ youtubeplay3.py
@@ -19,53 +19,29 @@ driver = webdriver.Chrome(options=chrome_options)
 # Counter to name the downloaded videos
 counter = 1
 
-# loop through pages 1 to 3
-for page in range(1, 3):
-    # URL da página desejada
-    url_vimeo = f"https://vimeo.com/search/page:{page}/sort:latest?duration=long&q=aula"
-    
-    # Abrir a página desejada
-    driver.get(url_vimeo)
-    
-    # Aguardar alguns segundos para carregar todo o conteúdo da página
-    time.sleep(5)
+for page in range(1, 11): # loop through pages 1 to 10
 
-    # Scroll to the bottom of the page using ActionChains
-    while True:
-        try:
-            # Find all <a> elements with class "iris_video-vital__overlay"
-            videos = driver.find_elements(By.CSS_SELECTOR, "a.iris_video-vital__overlay")
+    # Access the desired page
+    driver.get(f"https://vimeo.com/search/page:{page}/sort:latest?duration=long&q=aula")
 
-            # Store the links of the found videos
-            video_links = []
-            for video in videos:
-                video_links.append(video.get_attribute("href"))
+    # Find all <a> elements with class "iris_video-vital__overlay"
+    videos = driver.find_elements(By.CSS_SELECTOR, "a.iris_video-vital__overlay")
 
-            # Find all <span> elements with class "iris_link iris_link--gray-2"
-            video_titles = driver.find_elements(By.CSS_SELECTOR, "span.iris_link.iris_link--gray-2")
+    # Store the links of the found videos
+    video_links = []
+    for video in videos:
+        video_links.append(video.get_attribute("href"))
 
-            # Store the titles of the found videos
-            video_titles_list = []
-            for title in video_titles:
-                video_titles_list.append(title.get_attribute("title"))
+    # Find all <span> elements with class "iris_link iris_link--gray-2"
+    video_titles = driver.find_elements(By.CSS_SELECTOR, "span.iris_link.iris_link--gray-2")
 
-            # Dictionary with links and titles of the videos
-            video_dict = dict(zip(video_links, video_titles_list))
-        except:
-            break
-        
-        # Get the page source again after scrolling to the bottom
-        html_content = driver.page_source
-        time.sleep(5)
+    # Store the titles of the found videos
+    video_titles_list = []
+    for title in video_titles:
+        video_titles_list.append(title.get_attribute("title"))
 
-        # Find the links and titles of the videos found
-        try:
-            soup = BeautifulSoup(html_content, "html.parser")
-            videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-video-renderer")
-            links = ["https://www.youtube.com" + video.get("href") for video in videos]
-            titles = [video.get("title") for video in videos]
-        except Exception as e:
-            print(f"Erro: {e}")
+    # Dictionary with links and titles of the videos
+    video_dict = dict(zip(video_links, video_titles_list))
         finally:
             # Close the driver
             driver.quit()

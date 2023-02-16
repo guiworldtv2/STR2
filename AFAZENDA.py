b63c8565cde60b6ea9b7c5afcc27aa804dbe6a4a
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-15 22:01:39 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -40,24 +40,20 @@ while True:
 # Get the page source again after scrolling to the bottom
 html_content = driver.page_source
 
-# Find the links and titles of the videos found
+# Find the links, titles and thumbnails of the videos found
 try:
     soup = BeautifulSoup(html_content, "html.parser")
     videos = soup.find_all("a", class_="ScCoreLink-sc-16kq0mq-0 jKBAWW tw-link", href=True)
     links = ["https://www.twitch.tv" + video.get("href") for video in videos]
     channels = [video.find("p", {"data-a-target": "preview-card-channel-link", "class": "CoreText-sc-1txzju1-0 jiepBC"}).get("title") for video in videos]
     titles = [video.find("h3", class_="CoreText-sc-1txzju1-0 eJuFGD").get("title") for video in videos]
+    thumbnails = [video.find("img", class_="search-result-card__img tw-image").get("src") for video in videos]
 except Exception as e:
     print(f"Erro: {e}")
 finally:
     # Close the driver
     driver.quit()
 
-
-
-
-# Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
 
 # Get the playlist and write to file
 try:
@@ -70,7 +66,9 @@ try:
 
             # Write the stream information to the file
             title = channels[i]
-            f.write(f"#EXTINF:-1 tvg-id='{title}' group-title=\"TWITCH\",{title}\n")           
+            thumbnail = thumbnails[i]
+
+            f.write(f"#EXTINF:-1 tvg-id='{title}' tvg-logo='{thumbnail}' group-title=\"TWITCH\",{title}\n")           
             f.write(f"{url}\n")
             f.write("\n")
 except Exception as e:

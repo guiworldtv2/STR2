36c4cbed97817182ae2772ce5a0e88971a87f016
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-17 15:02:00 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -59,37 +59,22 @@ finally:
 
 
 # Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'youtube-dl'])
+subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
 
-
-time.sleep(5)
-
-# Define as opções para o youtube-dl
-ydl_opts = {
-    'format': 'best',  # Obtém a melhor qualidade
-    'merge_output_format': 'm3u8',  # Obtém a url com o formato .m3u8
-    'write_all_thumbnails': False,  # Não faz download das thumbnails
-    'skip_download': True,  # Não faz download do vídeo
-}
-
+# Get the playlist and write to file
 try:
-    with open('./YOUTUBEPLAY2.m3u', 'w') as f:
+    with open('./TWITCHPLAY.m3u', 'w') as f:
         f.write("#EXTM3U\n")  # Imprime #EXTM3U uma vez no início do arquivo
         for i, link in enumerate(links):
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                # Obtém informações do vídeo com o youtube-dl
-                info_dict = ydl.extract_info(link, download=False)
+            # Get the stream information using streamlink
+            streams = streamlink.streams(link)
+            url = streams['best'].url
 
-                # Obtém a url do vídeo com o formato .m3u8
-                url = info_dict['url']
+            # Write the stream information to the file
+            title = channels[i]
 
-                # Obtém a url da thumbnail do vídeo
-                thumbnail_url = info_dict['thumbnail']
-
-                # Escreve as informações do vídeo no arquivo .m3u8
-                title = titles[i]
-                f.write(f"#EXTINF:-1 group-title=\"YOUTUBE2\" tvg-logo=\"{thumbnail_url}\",{title}\n")
-                f.write(f"{url}\n\n")
-                f.write("\n")
+            f.write(f"#EXTINF:-1 tvg-id='{title}' tvg-logo=\"https://static-cdn.jtvnw.net/previews-ttv/live_user_{title}-1280x720.jpg\" group-title=\"TWITCH\",{title}\n")            
+            f.write(f"{url}\n")
+            f.write("\n")
 except Exception as e:
     print(f"Erro ao criar o arquivo .m3u8: {e}")

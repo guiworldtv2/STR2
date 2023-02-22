435bed4bd1a15334265c1cb673c1c213c0a0e196
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-22 02:11:43 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -1,80 +1,37 @@
-import streamlink
-import subprocess
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 import os
-from selenium import webdriver
+import subprocess
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-import youtube_dl
 
-# Configuring Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-
-# Instanciando o driver do Chrome
-driver = webdriver.Chrome(options=chrome_options)
 
 # URL da página desejada
-url_youtube = "https://www.youtube.com/results?search_query=podcast&sp=CAISAhgC"
+url_playplus = "https://www.playplus.com/live/liveEvent/249"
+
+# Criando as opções para o Google Chrome
+options = Options()
+ptions.add_argument(f"user-data-dir=/home/codespace/.config/google-chrome/MEU PROFILE")  # caminho do perfil do usuário
+options.add_argument("--start-maximized")  # maximizar a janela do navegador
+options.add_argument("--disable-infobars")  # desabilitar a barra de informações do Chrome
+
+# Instanciando o driver do Google Chrome
+driver = webdriver.Chrome(options=options)
 
 # Abrir a página desejada
-driver.get(url_youtube)
+driver.get(url_playplus)
 
 # Aguardar alguns segundos para carregar todo o conteúdo da página
 time.sleep(5)
 
-# Scroll to the bottom of the page using ActionChains
-while True:
-    try:
-        # Find the last video on the page
-        last_video = driver.find_element_by_xpath("//a[@class='ScCoreLink-sc-16kq0mq-0 jKBAWW tw-link'][last()]")
-        # Scroll to the last video
-        actions = ActionChains(driver)
-        actions.move_to_element(last_video).perform()
-        time.sleep(1)
-    except:
-        break
-        
-        
-# Get the page source again after scrolling to the bottom
+# Obter o conteúdo da página
 html_content = driver.page_source
 
-time.sleep(5)
+# Encontrar o link .m3u8 na página
+start_index = html_content.find("var urlLive = '") + 15
+end_index = html_content.find("';", start_index)
+link = html_content[start_index:end_index]
+print(link)
 
-# Find the links and titles of the videos found
-try:
-    soup = BeautifulSoup(html_content, "html.parser")
-    videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-video-renderer")
-    links = ["https://www.youtube.com" + video.get("href") for video in videos]
-    titles = [video.get("title") for video in videos]
-except Exception as e:
-    print(f"Erro: {e}")
-finally:
-    # Close the driver
-    driver.quit()
-
-
-
-
-# Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
-
-# Get the playlist and write to file
-try:
-    with open('./TWITCHPLAY.m3u', 'w') as f:
-        f.write("#EXTM3U\n")  # Imprime #EXTM3U uma vez no início do arquivo
-        for i, link in enumerate(links):
-            # Get the stream information using streamlink
-            streams = streamlink.streams(link)
-            url = streams['best'].url
-
-            # Write the stream information to the file
-            title = channels[i]
-
-            f.write(f"#EXTINF:-1 tvg-id='{title}' tvg-logo=\"https://static-cdn.jtvnw.net/previews-ttv/live_user_{title}-1280x720.jpg\" group-title=\"TWITCH\",{title}\n")            
-            f.write(f"{url}\n")
-            f.write("\n")
-except Exception as e:
-    print(f"Erro ao criar o arquivo .m3u8: {e}")
+# Fechar o driver
+driver.quit()

2014c1f1935565004303b90be2563a62bba6297c
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-22 04:01:19 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # Configuring Chrome options
 chrome_options = Options()
+chrome_options.add_argument("--window-size=1920,1080") # set screen size to 1920x1080
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 

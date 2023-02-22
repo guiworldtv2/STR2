6ab33dc4eddf9c7cb58ad2389be1ca4dda1e4bb4
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-22 02:30:35 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -4,21 +4,25 @@ import time
 import os
 import subprocess
 from bs4 import BeautifulSoup
+import streamlink
+import subprocess
+import time
+import os
+from selenium.webdriver.common.action_chains import ActionChains
 
+
+
+# Configuring Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument(f"user-data-dir=https://drive.google.com/drive/folders/1VkhVPGRZ0j5h937AwjpIKBAupKbESb83?usp=sharing")   # caminho do perfil do usuário
+
+# Instanciando o driver do Chrome
+driver = webdriver.Chrome(options=chrome_options)
 
 # URL da página desejada
 url_playplus = "https://www.playplus.com/live/liveEvent/249"
-
-# Criando as opções para o Google Chrome
-options = Options()
-options.add_argument(f"user-data-dir=https://drive.google.com/drive/folders/1VkhVPGRZ0j5h937AwjpIKBAupKbESb83?usp=sharing")   # caminho do perfil do usuário
-options.add_argument("--start-maximized")  # maximizar a janela do navegador
-options.add_argument("--disable-infobars")  # desabilitar a barra de informações do Chrome
-options.add_argument("--no-sandbox")
-
-
-# Instanciando o driver do Google Chrome
-driver = webdriver.Chrome(options=options)
 
 # Abrir a página desejada
 driver.get(url_playplus)

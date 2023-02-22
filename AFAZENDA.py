bdef1bdd1780797571ed48f487d9eb5a4f61a5f7
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-22 03:45:18 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -1,57 +1,39 @@
+import streamlink
+import subprocess
 import time
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-
-# instantiate the Chrome driver
-driver = webdriver.Chrome()
-
-# navigate to the target URL
-url_globoplay = "https://globoplay.globo.com/agora-na-tv/"
-driver.get(url_globoplay)
-
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Configuring Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 
+# Instanciando o driver do Chrome
+driver = webdriver.Chrome(options=chrome_options)
 
+# URL da página desejada
+url_twitch = "https://www.twitch.tv/jolyn"
 
-# Take 5 screenshots every 5 seconds
-for i in range(5):
-    driver.save_screenshot(f"screenshot{i+1}.png")
-    time.sleep(9)
-    
-botao_assistir_agora = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Assista agora')]")))
-botao_assistir_agora.click()
-time.sleep(5)
-
-campo_email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
-campo_senha = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
-
-campo_email.send_keys("bundadalele@gmail.com")
-campo_senha.send_keys("minhasenhaéfoda97")
-
-botao_entrar = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Entrar')]")))
-botao_entrar.click()
+# Abrir a página desejada
+driver.get(url_twitch)
 
 # Aguardar alguns segundos para carregar todo o conteúdo da página
-time.sleep(10)
+time.sleep(5)
 
-# Get the page source to find the .m3u8 link
+# Obter o arquivo .m3u8
+# Pegando o log de requisições da aba Rede
 log_entries = driver.execute_script("return window.performance.getEntries();")
 
+# Buscando a primeira requisição que tem um arquivo .m3u8
 for entry in log_entries:
     if ".m3u8" in entry['name']:
+        print(entry['name'])
         link = entry['name']
         break
 
-# Close the driver
+# Fechando o driver
 driver.quit()

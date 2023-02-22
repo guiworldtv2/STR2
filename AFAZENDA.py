4b774ee5870785f36c2c44fd9d029ca65f810791
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-22 03:30:21 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+# Define o serviço para o driver
+service = ChromeService(executable_path='caminho/para/o/driver/chromedriver.exe')
 
+# Inicializa o driver
 driver = webdriver.Chrome(service=service)
 
-
+# Abre a página
 driver.get("https://globoplay.globo.com/agora-na-tv/")
-time.sleep(5)
 
 # Take 5 screenshots every 5 seconds
 for i in range(5):

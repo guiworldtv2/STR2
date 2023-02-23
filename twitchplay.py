6bd0225e6ed62983814c1a324ffd89d0c63c4806
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-23 03:27:00 -0300

    Update twitchplay.py

--- twitchplay.py
+++ twitchplay.py
@@ -24,26 +24,7 @@ driver.get(url_twitch)
 # Aguardar alguns segundos para carregar todo o conteúdo da página
 time.sleep(5)
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-
-# Define o seletor CSS do elemento que contém a lista de itens
-lista_css_selector = ".tw-tower > div"
-
-# Espera até que o elemento da lista esteja presente e visível
-elemento_lista = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, lista_css_selector))
-)
-WebDriverWait(driver, 10).until(
-    EC.visibility_of(elemento_lista)
-)
-
-# Rola a página para baixo até o final da lista
-ultimo_item_css_selector = f"{lista_css_selector}:last-child"
-ultimo_item = driver.find_element(By.CSS_SELECTOR, ultimo_item_css_selector)
-driver.execute_script("arguments[0].scrollIntoView();", ultimo_item)
         
 # Get the page source again after scrolling to the bottom
 html_content = driver.page_source

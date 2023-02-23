13b8d6409236896b477fc3e244c295714e2dee07
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-23 18:02:38 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -6,28 +6,51 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # URL da página de login
-url_playplus_login = "https://www.rtve.es/play/videos/directo/canales-lineales/la-1/"
+url_playplus_login = "https://www.playplus.com/Account/Login"
 
 # Criando as opções para o chrome
+options = Options()
+options.add_argument("--window-size=1920,1080") # set screen size to 1920x1080
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
 
-chrome_options = Options()
-chrome_options.add_argument("--window-size=1920,1080") # set screen size to 1920x1080
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-
-# Instanciando o driver do Chrome
-driver = webdriver.Chrome(options=chrome_options)
+# Instanciando o driver do chrome
+driver = webdriver.Chrome(options=options)
 
 # Abrir a página de login do PlayPlus
 driver.get(url_playplus_login)
 
+# Take 5 screenshots every 5 seconds
+for i in range(5):
+    driver.save_screenshot(f"screenshot{i+1}.png")
+    time.sleep(9)
+    
+    # Aguardar alguns segundos para carregar todo o conteúdo da página
+time.sleep(15)
 
+# Esperar até que os campos de e-mail e senha estejam presentes na página
+email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "UserName")))
+
+# Preencher o campo de e-mail com a informação desejada
+email_field.send_keys("{EMAILPLAYPLUS}")
+
+# Preencher o campo de senha com a informação desejada
+password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Password")))
+password_field.send_keys("{SENHAPLAYPLUS}")
+
+# Localizar e clicar no botão "Avançar"
+login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='redirect btn btn-primary btn-block btn-login btn-next']")))
+login_button.click()
+
+# Localizar e clicar no botão do perfil do usuário
+profile_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='profile redirect']")))
+profile_button.click()
 
 # Esperando a página ser carregada após clicar no perfil do usuário
 time.sleep(10)
 
 # URL da página desejada para extração da .m3u8
-url_playplus = "https://www.rtve.es/play/videos/directo/canales-lineales/la-1/"
+url_playplus = "https://www.playplus.com/live/liveEvent/249"
 
 # Abrir a página desejada após o login
 driver.get(url_playplus)
@@ -38,15 +61,11 @@ time.sleep(5)
 # Obter o conteúdo da página
 html_content = driver.page_source
 
-# Pegando o log de requisições da aba Rede
-log_entries = driver.execute_script("return window.performance.getEntries();")
-
-# Buscando a primeira requisição que tem um arquivo .m3u8
-for entry in log_entries:
-    if ".m3u8" in entry['name']:
-        print(entry['name'])
-        link = entry['name']
-        break
+# Encontrar o link .m3u8 na página
+start_index = html_content.find("var urlLive = '") + 15
+end_index = html_content.find("';", start_index)
+link = html_content[start_index:end_index]
+print(link)
 
 # Fechar o driver
 driver.quit()

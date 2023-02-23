8b817fadd3e8590a644476edaab750b3d90fe5fb
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-23 04:02:12 -0300

    Update BBVIPALBANIA.py

--- BBVIPALBANIA.py
+++ BBVIPALBANIA.py
@@ -5,13 +5,10 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 
-
-
-
 # URL da página desejada
 url_twitch = "https://www.twitch.tv/search?term=big%20brother"
 
- # Configuring Chrome options
+# Configurando Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
@@ -28,27 +25,37 @@ time.sleep(5)
 # Obter o conteúdo da página
 html_content = driver.page_source
 
-# Encontrar o link do primeiro canal encontrado
+# Encontrar os links dos canais que correspondem ao critério desejado
 soup = BeautifulSoup(html_content, "html.parser")
-link = "https://www.twitch.tv" + soup.find("a", class_="ScCoreLink-sc-16kq0mq-0 eYjhIv tw-link").get("href")
+channel_links = []
+for channel in soup.find_all("strong", {"data-test-selector": "search-result-live-channel__name"}):
+    if channel.a is not None:
+        channel_links.append("https://www.twitch.tv" + channel.a.get("href"))
 
 # Fechar o driver
 driver.quit()
 
-print(link)
+# Verificar se foram encontrados links
+if not channel_links:
+    print("Nenhum canal encontrado.")
+else:
+    # Escolher o primeiro canal encontrado
+    link = channel_links[0]
 
-# Instalando streamlink
-subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
+    # Instalando streamlink
+    subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
 
-    
-try:
-    # Get LISTA4.m3u8
-    with open('./BBVIPALBANIA.m3u8', 'w') as f:
-        streams = streamlink.streams(link)
-        url = streams['best'].url
-        f.write("#EXTM3U\n")
-        f.write("#EXT-X-VERSION:3\n")
-        f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n")
-        f.write(f"{url}\n")
-except Exception as e:
-    print(f"Erro ao criar o arquivo .m3u8: {e}")
+    try:
+        # Get LISTA4.m3u8
+        with open('./BBVIPALBANIA.m3u8', 'w') as f:
+            streams = streamlink.streams(link)
+            if 'best' in streams:
+                url = streams['best'].url
+                f.write("#EXTM3U\n")
+                f.write("#EXT-X-VERSION:3\n")
+                f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n")
+                f.write(f"{url}\n")
+            else:
+                print("Não foi possível encontrar a stream de melhor qualidade.")
+    except Exception as e:
+        print(f"Erro ao criar o arquivo .m3u8: {e}")

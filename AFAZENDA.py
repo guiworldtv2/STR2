6a5ee2a6af21891f6c0e220bb402e98f5145b1db
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-24 01:09:54 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -8,7 +8,7 @@ import os
 import secrets 
 
 # URL da página de login
-url_playplus_login = "https://www.playplus.com/Account/Login"
+url_playplus_login = "https://vimeo.com/598201688"
 
 # Criando as opções para o chrome
 options = Options()
@@ -27,26 +27,6 @@ for i in range(5):
     driver.save_screenshot(f"screenshot{i+1}.png")
     time.sleep(9)
 
-# Aguardar alguns segundos para carregar todo o conteúdo da página
-time.sleep(15)
-
-# Esperar até que os campos de e-mail e senha estejam presentes na página
-email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "UserName")))
-
-# Preencher o campo de e-mail com a informação desejada
-email_field.send_keys("oquefoiagora@hotmail.com")
-
-# Preencher o campo de senha com a informação desejada
-password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Password")))
-password_field.send_keys("ratosdeporao062")
-
-# Localizar e clicar no botão "Avançar"
-login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='redirect btn btn-primary btn-block btn-login btn-next']")))
-login_button.click()
-
-# Localizar e clicar no botão do perfil do usuário
-profile_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='profile redirect']")))
-profile_button.click()
 
 # Esperando a página ser carregada após clicar no perfil do usuário
 time.sleep(10)
@@ -55,57 +35,21 @@ time.sleep(10)
 time.sleep(10)
 #URL da página desejada para extração da .m3u8
 
-url_playplus = "https://www.playplus.com/live"
+url_playplus = "https://vimeo.com/598201688"
 #Abrir a página desejada após o login
 
 driver.get(url_playplus)
 #Esperando a página ser carregada
 
-time.sleep(10)
+# Obter o conteúdo da página
+html_content = driver.page_source
 
-# Encontrar todos os links para os eventos ao vivo
-live_links = driver.find_elements_by_xpath("//a[@class='no-decoration']")
+# Pegando o log de requisições da aba Rede
+log_entries = driver.execute_script("return window.performance.getEntries();")
 
-# Criar uma lista para armazenar os links dos canais
-channel_links = []
-
-# Para cada link, obter as informações necessárias
-for link in live_links:
-    # Obter o ID do evento ao vivo
-    event_id = link.get_attribute("href").split("/")[-1]
-
-    # Obter o subtema do canal
-    subtheme = link.find_element_by_xpath(".//div[@class='pl-2 pr-2 mb-0 span-view title now']")
-    subtheme = subtheme.text.strip()
-
-    # Obter o nome do canal
-    channel_name = link.find_element_by_xpath(".//img[@class='live-slider img-fluid thumbnail m-auto d-block now']")
-    channel_name = channel_name.get_attribute("data-sname")
-
-    # Obter a URL do evento ao vivo
-    url = f"https://www.playplus.com/live/{event_id}"
-
-    # Abrir a página do evento ao vivo
-    driver.get(url)
-
-    # Obter o conteúdo da página
-    html_content = driver.page_source
-
-    # Encontrar o link .m3u8 na página
-    start_index = html_content.find("var urlLive = '") + 15
-    end_index = html_content.find("';", start_index)
-    link = html_content[start_index:end_index]
-
-    # Adicionar o link na lista de links de canais
-    channel_links.append((channel_name, "https://www.playplus.com" + link))
-
-# Fechar o driver
-driver.quit()
-
-# Criar o arquivo .m3u8 com os links dos canais
-with open("playplus_channels.m3u8", "w") as f:
-    # Escrever o cabeçalho do arquivo
-    f.write("#EXTM3U\n")
-    for channel_link in channel_links:
-        # Escrever o link do canal no formato EXTINF
-        f.write(f"#EXTINF:-1,tvg-logo=\"{channel_link[0]}\",{channel_link[0]} - {channel_link[1]}\n{channel_link[1]}\n")
+# Buscando a primeira requisição que tem um arquivo .m3u8
+for entry in log_entries:
+    if ".m3u8" in entry['name']:
+        print(entry['name'])
+        link = entry['name']
+        break

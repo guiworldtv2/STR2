cb539c438b4518276737c84c73d7cbb13cc37cda
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-23 22:21:17 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -4,17 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
+import secrets 
 
 # URL da página de login
 url_playplus_login = "https://www.playplus.com/Account/Login"
 
-# Configuração do driver
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-
+# Criando as opções para o chrome
+options = Options()
+options.add_argument("--window-size=1920,1080") # set screen size to 1920x1080
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
 
 # Instanciando o driver do chrome
 driver = webdriver.Chrome(options=options)
@@ -51,53 +51,27 @@ profile_button.click()
 # Esperando a página ser carregada após clicar no perfil do usuário
 time.sleep(10)
 
+# URL da página desejada para extração da .m3u8
+url_playplus = "https://www.playplus.com/live/liveEvent/249"
 
-# URL da página de transmissões ao vivo
-url_live = "https://www.playplus.com/live"
+# Abrir a página desejada após o login
+driver.get(url_playplus)
 
-
-# Esperar o login ser efetuado
+# Esperando o vídeo ser carregado e reproduzido
 time.sleep(5)
 
-# Acessar a página de transmissões ao vivo
-driver.get(url_live)
+# Obter o conteúdo da página
+html_content = driver.page_source
 
-# Esperar a página ser carregada
-time.sleep(5)
-
-# Extrair informações de cada transmissão
-transmissions = driver.find_elements_by_xpath("//a[contains(@class, 'no-decoration') and contains(@href, '/live/liveEvent/')]")
-
-for transmission in transmissions:
-    # Extrair informações da transmissão
-    name = transmission.find_element_by_xpath(".//div[contains(@class, 'span-view') and contains(@class, 'title')]").text
-    thumbnail = transmission.find_element_by_xpath(".//img").get_attribute("src")
-    link = "https://www.playplus.com" + transmission.get_attribute("href")
-    
-    # Acessar a página da transmissão
-    driver.get(link)
-
-    # Esperar o vídeo ser carregado e reproduzido
-    time.sleep(5)
-
-    # Obter o conteúdo da página
-    html_content = driver.page_source
-
-    # Encontrar o link .m3u8 na página
-    start_index = html_content.find("var urlLive = '") + 15
-    end_index = html_content.find("';", start_index)
-    m3u8_link = html_content[start_index:end_index]
-
-    # Gerar arquivo .m3u8 para a transmissão
-    file_name = name.replace(" ", "_").lower() + ".m3u8"
-    with open(file_name, "w") as f:
-        f.write("#EXTM3U\n#EXTINF:-1," + name + "\n" + m3u8_link)
-
-    # Imprimir o link da thumbnail
-    print("Transmissão:", name)
-    print("Link da thumbnail:", thumbnail)
-    print("Link .m3u8:", m3u8_link)
-    print()
+# Encontrar o link .m3u8 na página
+start_index = html_content.find("var urlLive = '") + 15
+end_index = html_content.find("';", start_index)
+link = html_content[start_index:end_index]
+print(link)
 
 # Fechar o driver
 driver.quit()
+
+import os
+os.environ["EMAILPLAYPLUS"] = "seu-email-aqui"
+print(os.getenv("EMAILPLAYPLUS"))

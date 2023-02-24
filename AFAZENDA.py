4d7633ae803cd8320d366c031cccb4981c393ba0
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-23 22:10:50 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -30,19 +30,15 @@ for i in range(5):
 # Aguardar alguns segundos para carregar todo o conteúdo da página
 time.sleep(15)
 
-import os
-
-# Obter o e-mail e senha das variáveis de ambiente
-email = os.environ.get('EMAILPLAYPLUS')
-password = os.environ.get('SENHAPLAYPLUS')
+# Esperar até que os campos de e-mail e senha estejam presentes na página
+email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "UserName")))
 
 # Preencher o campo de e-mail com a informação desejada
-email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "UserName")))
-email_field.send_keys(email)
+email_field.send_keys("oquefoiagora@hotmail.com")
 
 # Preencher o campo de senha com a informação desejada
 password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Password")))
-password_field.send_keys(password)
+password_field.send_keys("ratosdeporao062")
 
 # Localizar e clicar no botão "Avançar"
 login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='redirect btn btn-primary btn-block btn-login btn-next']")))
@@ -55,28 +51,70 @@ profile_button.click()
 # Esperando a página ser carregada após clicar no perfil do usuário
 time.sleep(10)
 
-# URL da página desejada para extração da .m3u8
-url_playplus = "https://www.playplus.com/live/liveEvent/249"
 
-# Abrir a página desejada após o login
-driver.get(url_playplus)
+# URL da página de transmissões ao vivo
+url_live = "https://www.playplus.com/live"
 
-# Esperando o vídeo ser carregado e reproduzido
+# Informações de login
+email = "seu-email-aqui"
+password = "sua-senha-aqui"
+
+# Configuração do driver
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=options)
+
+# Fazer login
+driver.get(url_login)
+driver.find_element_by_id("login").send_keys(email)
+driver.find_element_by_id("password").send_keys(password)
+driver.find_element_by_xpath("//button[contains(text(),'Entrar')]").click()
+
+# Esperar o login ser efetuado
 time.sleep(5)
 
-# Obter o conteúdo da página
-html_content = driver.page_source
+# Acessar a página de transmissões ao vivo
+driver.get(url_live)
 
-# Encontrar o link .m3u8 na página
-start_index = html_content.find("var urlLive = '") + 15
-end_index = html_content.find("';", start_index)
-link = html_content[start_index:end_index]
-print(link)
+# Esperar a página ser carregada
+time.sleep(5)
+
+# Extrair informações de cada transmissão
+transmissions = driver.find_elements_by_xpath("//a[contains(@class, 'no-decoration') and contains(@href, '/live/liveEvent/')]")
+
+for transmission in transmissions:
+    # Extrair informações da transmissão
+    name = transmission.find_element_by_xpath(".//div[contains(@class, 'span-view') and contains(@class, 'title')]").text
+    thumbnail = transmission.find_element_by_xpath(".//img").get_attribute("src")
+    link = "https://www.playplus.com" + transmission.get_attribute("href")
+    
+    # Acessar a página da transmissão
+    driver.get(link)
+
+    # Esperar o vídeo ser carregado e reproduzido
+    time.sleep(5)
+
+    # Obter o conteúdo da página
+    html_content = driver.page_source
+
+    # Encontrar o link .m3u8 na página
+    start_index = html_content.find("var urlLive = '") + 15
+    end_index = html_content.find("';", start_index)
+    m3u8_link = html_content[start_index:end_index]
+
+    # Gerar arquivo .m3u8 para a transmissão
+    file_name = name.replace(" ", "_").lower() + ".m3u8"
+    with open(file_name, "w") as f:
+        f.write("#EXTM3U\n#EXTINF:-1," + name + "\n" + m3u8_link)
+
+    # Imprimir o link da thumbnail
+    print("Transmissão:", name)
+    print("Link da thumbnail:", thumbnail)
+    print("Link .m3u8:", m3u8_link)
+    print()
 
 # Fechar o driver
 driver.quit()
-
-import os
-
-senha = os.environ['SENHA']
-print(senha)

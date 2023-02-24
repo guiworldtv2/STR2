1281f8c87cc7fc13b29871dcfb8b26a77ad0491b
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-24 00:23:41 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -51,27 +51,63 @@ profile_button.click()
 # Esperando a página ser carregada após clicar no perfil do usuário
 time.sleep(10)
 
-# URL da página desejada para extração da .m3u8
-url_playplus = "https://www.playplus.com/live/liveEvent/249"
+Esperando a página ser carregada após o login
 
-# Abrir a página desejada após o login
+time.sleep(10)
+URL da página desejada para extração da .m3u8
+
+url_playplus = "https://www.playplus.com/live"
+Abrir a página desejada após o login
+
 driver.get(url_playplus)
+Esperando a página ser carregada
 
-# Esperando o vídeo ser carregado e reproduzido
-time.sleep(5)
+time.sleep(10)
+Encontrar todos os links para os eventos ao vivo
 
-# Obter o conteúdo da página
-html_content = driver.page_source
+live_links = driver.find_elements_by_xpath("//a[@class='no-decoration']")
+Criar um dicionário para armazenar os links e subtemas dos canais
 
-# Encontrar o link .m3u8 na página
-start_index = html_content.find("var urlLive = '") + 15
-end_index = html_content.find("';", start_index)
-link = html_content[start_index:end_index]
-print(link)
+channel_links = {}
+Para cada link, obter as informações necessárias
+
+for link in live_links:
+# Obter o ID do evento ao vivo
+event_id = link.get_attribute("href").split("/")[-1]
+
+# Obter o subtema do canal
+subtheme = link.find_element_by_xpath(".//div[@class='pl-2 pr-2 mb-0 span-view title now']")
+subtheme = subtheme.text.strip()
+
+# Obter o nome do canal
+channel_name = link.find_element_by_xpath(".//img[@class='live-slider img-fluid thumbnail m-auto d-block now']")
+channel_name = channel_name.get_attribute("data-sname")
+
+# Iterando sobre as URLs dos eventos ao vivo
+for url in url_list:
+    # Abrir a página do evento ao vivo
+    driver.get(url)
+
+    # Obter o conteúdo da página
+    html_content = driver.page_source
+
+    # Encontrar o link .m3u8 na página
+    start_index = html_content.find("var urlLive = '") + 15
+    end_index = html_content.find("';", start_index)
+    link = html_content[start_index:end_index]
+    channel_name = html_content.find("data-sname=\"") + 12
+    channel_name_end = html_content.find("\"", channel_name)
+    channel_name = html_content[channel_name:channel_name_end]
+
+    # Adicionar o link na lista de links de canais
+    channel_links.append((channel_name, "https://www.playplus.com" + link))
 
 # Fechar o driver
 driver.quit()
-
-import os
-os.environ["EMAILPLAYPLUS"] = "seu-email-aqui"
-print(os.getenv("EMAILPLAYPLUS"))
+# Criar o arquivo .m3u8 com os links dos canais
+with open("playplus_channels.m3u8", "w") as f:
+    # Escrever o cabeçalho do arquivo
+    f.write("#EXTM3U\n")
+    for channel_link in channel_links:
+        # Escrever o link do canal no formato EXTINF
+        f.write(f"#EXTINF:-1,tvg-logo=\"{channel_link[0]}\",{channel_link[0]} - {channel_link[1]}\n{channel_link[1]}\n")

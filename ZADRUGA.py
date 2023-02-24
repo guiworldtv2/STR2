7c08cb72cd7f2c911823ca0f42f07a1a056373ed
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-24 06:50:08 -0300

    Update ZADRUGA.py

--- ZADRUGA.py
+++ ZADRUGA.py
@@ -1,47 +1,34 @@
-#! /usr/bin/python3
-
 from __future__ import unicode_literals
-import youtube_dl
 import requests
 import shutil
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from pytube import YouTube
+
 channel_no = 0
 m3u = None
-def get_live_info(channel_id):
-    try:
-        webpage = urlopen(f"{channel_id}").read()
-        soup = BeautifulSoup(webpage, 'html.parser')
-        urlMeta = soup.find("meta", property="og:url")
-        if urlMeta is None:
-            return None
-        url = urlMeta.get("content")
-        if(url is None or url.find("/watch?v=") == -1):
-            return None
-        titleMeta = soup.find("meta", property="og:title")
-        imageMeta = soup.find("meta", property="og:image")
-        descriptionMeta = soup.find("meta", property="og:description")
-        return {
-            "url": url,
-            "title": titleMeta.get("content"),
-            "image": imageMeta.get("content"),
-            "description": descriptionMeta.get("content")
-        }
-    
-    except Exception as e:
-                return None
 
-banner = r'''
+def get_video_info(video_url):
+    try:
+        yt = YouTube(video_url)
+        return {
+            "url": video_url,
+            "title": yt.title,
+            "image": yt.thumbnail_url,
+            "description": yt.description
+        }
+    except Exception as e:
+        return None
+
+banner = '''#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5400000
 '''
 
-def generate_youtube_tv():
-    global channel_no
-    ydl_opts = {
-        'format': 'best',
-    }
-    ydl = youtube_dl.YoutubeDL(ydl_opts)
+def generate_youtube_playlist():
+    global channel_no, m3u
+    m3u = open("ZADRUGA_playlist.m3u8", "w")
+    m3u.write(banner)
 
     with open('ZADRUGA.txt') as f:
         lines = f.readlines()
@@ -49,67 +36,24 @@ def generate_youtube_tv():
             line = line.strip()
             if line == "":
                 continue
-            channel = get_live_info(line)
+            channel = get_video_info(line)
             if channel is None:
                 continue
             try:
-                with ydl:
-                    result = ydl.extract_info(
-                        f"{line}",
-                        download=False  # We just want to extract the info
-                    )
-
-                    if 'entries' in result:
-                        # Can be a playlist or a list of videos
-                        video = result['entries'][-1]
-                    else:
-                        # Just a video
-                        video = result
-                video_url = video['url']
-
+                video = get_video_info(line)
+                if video is None:
+                    continue
+                video_link = video['url']
                 channel_no += 1
                 channel_name = f"{channel_no}-{line.split('/')[-1]}"
                 playlistInfo = f"#EXTINF:-1 tvg-chno=\"{channel_no}\" tvg-id=\"{line}\" tvg-name=\"{channel_name}\" tvg-logo=\"{channel.get('image')}\" group-title=\"ZADRUGA\",{channel.get('title')}\n"                
-                write_to_playlist(video_url)
-                write_to_playlist("\n")
+                m3u.write(playlistInfo)
+                m3u.write(video_link)
+                m3u.write("\n")
             except Exception as e:
                 print(e)
-                        
 
-
-
-def write_to_playlist(content):
-    global m3u    
-    m3u.write(content)
-    
-
-def create_playlist():
-    global m3u
-    m3u = open("ZADRUGA.m3u8", "w")
-    m3u.write("#EXTM3U")
-    m3u.write("\n")
-
-    
-def close_playlist():
-    global m3u
     m3u.close()
-def generate_youtube_PlayList():
-    create_playlist()
-        
-    m3u.write(banner)
 
-    generate_youtube_tv()
-    
-
-    
-
-    
-
-
-    close_playlist()
-
-
-    
 if __name__ == '__main__':
-    generate_youtube_PlayList()   
- 
+    generate_youtube_playlist()
